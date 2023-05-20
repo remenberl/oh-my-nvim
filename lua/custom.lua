189961707105b0974e89a901dc5c8903f32fe9b9
Jialu Liu <remenberl@gmail.com>
2023-05-20 18:05:48 -0400

    Adds personal cofig.
    - Emacs-like commands
    - close_if_last_window=true for neo-tree

---
+++ lua/custom.lua
@@ -0,0 +1,6 @@
+-- Emacs settings
+vim.api.nvim_set_keymap('i', '<C-a>', '<Esc>^i', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-a>', '^i', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-e>', '<Esc>A', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-e>', 'A', { noremap = true, silent = true })
+
